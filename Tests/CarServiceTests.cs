18e5f694e9002ee436b4f80ae003d3528b872aed
Slavic-man <jelevjani@gmail.com>
2026-04-04 17:03:49 +0300

    Added car service tests

--- Tests/CarServiceTests.cs
+++ Tests/CarServiceTests.cs
@@ -19,5 +19,56 @@ public class CarServiceTests
 		_mockRequestRepository = new Mock<IRepository<Request>>();
 		_carService = new CarService(_mockCarRepository.Object, _mockRequestRepository.Object);
 	}
-	
+	[Fact]
+    public void GetAllProjected_WithValidCars_ReturnsCarListProjections()
+    {
+        // Arrange
+        var cars = new List<Car>
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Make = "Toyota",
+                Model = "Camry",
+                Year = 2024,
+                Seats = 5,
+                PricePerDay = 50m,
+                ImagePath = "/images/camry.jpg"
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Make = "Honda",
+                Model = "Civic",
+                Year = 2023,
+                Seats = 5,
+                PricePerDay = 45m,
+                ImagePath = "/images/civic.jpg"
+            }
+        };
+
+        _mockCarRepository.Setup(r => r.GetMany(
+            It.IsAny<System.Linq.Expressions.Expression<System.Func<Car, bool>>>(),
+            It.IsAny<System.Linq.Expressions.Expression<System.Func<Car, CarListProjection>>>()))
+            .Returns(cars.Select(c => new CarListProjection
+            {
+                Id = c.Id,
+                Make = c.Make,
+                Model = c.Model,
+                Year = c.Year,
+                Seats = c.Seats,
+                PricePerDay = c.PricePerDay,
+                ImagePath = c.ImagePath
+            }).AsEnumerable());
+
+        // Act
+        var result = _carService.GetAllProjected().ToList();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(2, result.Count);
+        Assert.Equal("Toyota", result[0].Make);
+        Assert.Equal("Honda", result[1].Make);
+    }
+
 }
